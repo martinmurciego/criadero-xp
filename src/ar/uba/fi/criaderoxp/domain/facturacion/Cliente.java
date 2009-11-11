da4f0e6fea7c4b260973c05d64190cb7dc842fdc
matiasmazzei <matiasmazzei@330e2d5a-c9b5-11de-8410-e5b1e99fc78e>
2009-11-11 03:43:38 +0000

    + Elimino todas las clases antiguas, limpiando el proyecto. - (parte 3 del commit, porque fallaba).

--- src/ar/uba/fi/criaderoxp/domain/facturacion/Cliente.java
+++
@@ -1,7 +0,0 @@
-package ar.uba.fi.criaderoxp.domain.facturacion;
-
-public class Cliente extends Persona {
-	public Cliente(TipoDocumento tipoDocumento, String documento) {
-		super(tipoDocumento, documento);
-	}
-}
