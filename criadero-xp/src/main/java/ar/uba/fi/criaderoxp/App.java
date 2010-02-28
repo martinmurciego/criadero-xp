06d03c535dec22d9e0940a35ae43ce04f3c30f09
matiasmazzei <matiasmazzei@330e2d5a-c9b5-11de-8410-e5b1e99fc78e>
2010-02-28 18:24:33 +0000

    + Probando si se puede comitear con la nueva estructura de directorios o, por el contrario, rompí todo.

--- criadero-xp/src/main/java/ar/uba/fi/criaderoxp/App.java
+++ criadero-xp/src/main/java/ar/uba/fi/criaderoxp/App.java
@@ -1,13 +1,10 @@
 package ar.uba.fi.criaderoxp;
 
 /**
- * Hello world!
- *
+ * Hola mundo!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("¡Hola mundo!");
+	}
 }
