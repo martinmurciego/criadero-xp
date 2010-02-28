a8b275bcff122862de670ba84a155e94f06c5d57
matiasmazzei <matiasmazzei@330e2d5a-c9b5-11de-8410-e5b1e99fc78e>
2010-02-28 21:45:51 +0000

    + Renombrando el módulo del modelo hacia criadero-model

--- criadero-xp/src/test/java/ar/uba/fi/criaderoxp/domain/service/ServiceTestSuite.java
+++
@@ -1,9 +0,0 @@
-package ar.uba.fi.criaderoxp.domain.service;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses( { ConejoServiceTest.class })
-public class ServiceTestSuite {
-}
